7155dcde9e5be773b94ddb3dcf78e222bf83295f
hyunzi <jhi271271@gmail.com>
2024-01-15 18:12:36 +0900

    Week 2 - BipartiteGraph (45 / 81 testcases passed)

--- src/main/java/com/example/codingtest/W2BipartiteGraph.java
+++ src/main/java/com/example/codingtest/W2BipartiteGraph.java
@@ -4,8 +4,6 @@ import java.util.*;
 
 public class W2BipartiteGraph {
     public static boolean isBipartite(int[][] graph) {
-
-
         //이분그래프 판단 방법
         //0번째 노드에서 시작해서, DFS로 탐색
         //0번째를 빨간색으로 칠하고
@@ -17,29 +15,40 @@ public class W2BipartiteGraph {
         //노드 0에서 시작, visited[]에 진행할 노드의 색을 바꾼다
         //그 다음 노드는!
         String[] visited = new String[graph.length];
-        bfs(0, graph, visited, "red");
-
-
-        return true;
-    }
-
-    public static void bfs(int currNode, int[][] graph, String[] visited, String type) {
-        visited[currNode] = "red";
-        System.out.println(Arrays.toString(graph[currNode]));
-        for(int nextNode : graph[currNode]) {
-            if (visited[nextNode] != null && visited[currNode] != null
-                && visited[nextNode].equals(visited[currNode])) {
-                //이미 방문했던 노드
-                return;
-            } else {
-                type = (type == "red") ? "blue" : "red"; //반대의 타입을 넣어줌
-                bfs(nextNode, graph, visited, type);
-            }
-        }
+        boolean result = bfs(0, graph, visited, "red", true);
         System.out.println(Arrays.toString(visited));
+        System.out.println(result);
+
+        return result;
     }
+
+    public static boolean bfs(int currNode, int[][] graph, String[] visited, String type, boolean result) {
+
+        visited[currNode] = type;
+        for(int nextNode : graph[currNode]) {
+            if (visited[nextNode] != null) {
+                //이미 방문했던 노드
+                if (visited[nextNode].equals(type)) {
+                    //바꿀 색이랑 같으면? 정상
+                    result = true;
+                } else {
+                    result = false;
+                    //바꿀 색이랑 다르면? 이분그래프 안됨
+                }
+            } else {
+                //방문하지 않은 노드
+                type = (type == "red") ? "blue" : "red"; //반대의 타입을 넣어줌
+                result = bfs(nextNode, graph, visited, type, result);
+            }
+
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
 
-        isBipartite(new int[][]{{1,3},{0,2},{1,3},{0,2}});
+        isBipartite(new int[][]{{4,1},{0,2},{1,3},{2,4},{3,0}});
+        isBipartite(new int[][]{{1,2,3},{0,2},{0,1,3},{0,2}});
     }
 }
