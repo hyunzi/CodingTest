bf17b764eed47408ba040425abcbd6a3da2d6fdf
hyunzi <jhi271271@gmail.com>
2024-01-17 12:43:36 +0900

    Week 3 - Shortest Path In Binary Matrix BFS로 변경, RND3 파일 추가

---
+++ src/main/java/com/example/codingtest/W3RND3.java
@@ -0,0 +1,6 @@
+package com.example.codingtest;
+
+public class W3RND3 {
+    /*https://www.acmicpc.net/problem/17142*/
+
+}
--- src/main/java/com/example/codingtest/W3ShortestPathInBinaryMatrix.java
+++ src/main/java/com/example/codingtest/W3ShortestPathInBinaryMatrix.java
@@ -1,16 +1,15 @@
 package com.example.codingtest;
 
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 
 public class W3ShortestPathInBinaryMatrix {
 
     public static int shortestPathBinaryMatrix(int[][] grid) {
-        //BFS나 DFS로 풀 수 있음
-        //8개의 방향을 보며 탐색함
         //왼쪽 위에서 오른쪽 아래의 노드로 이동하는 최단거리 구하기
-        //0번 노드부터, 8개의 방향을 탐색함. 1이 있는 쪽으로 이동하여 DFS
-        //오른쪽 아래를 만났을 때 이동한 거리를 출력
-        //거리가 가장 짧은 걸 출력
+        //DFS 로도 풀 수 있으나 BFS로 풀어야 효율적임
+        //8개의 방향을 보며 탐색함
+        //0번 노드부터, 8개의 방향을 탐색함. 1이 있는 곳을 모두 탐색
+        //오른쪽 아래를 처음 만났을 때 이동한 거리를 리턴하면 됨
 
         int rowLength = grid.length;
         int colLength = grid[0].length;
@@ -21,44 +20,42 @@ public class W3ShortestPathInBinaryMatrix {
         }
 
         boolean[][] visited = new boolean[rowLength][colLength];
-        ArrayList<Integer> pathList = new ArrayList<Integer>();
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        int[] dr = {0,1,1,1,0,-1,-1,-1};
+        int[] dc = {1,1,0,-1,-1,-1,0,1};
 
-                dfs(i,j,grid,visited,pathList);
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{0,0,1});
+        visited[0][0] = true;
+
+        while (!queue.isEmpty()) {
+            int[] curPos = queue.poll();
+            int curRow = curPos[0];
+            int curCol = curPos[1];
+            int curLength = curPos[2];
+
+            if (curRow == rowLength - 1 && curCol == colLength - 1) {
+                result = curLength;
+                return result;
+            }
+            for (int i = 0; i < 8; i++) {
+                int nextRow = curRow + dr[i];
+                int nextCol = curCol + dc[i];
+                if (nextRow >= 0 && nextRow <= rowLength-1 && nextCol >= 0 && nextCol <= colLength - 1) {
+                    System.out.println(nextRow+","+nextCol+" 탐색");
+                    if (grid[nextRow][nextCol] == 0 && !visited[nextRow][nextCol]) {
+                        queue.offer(new int[]{nextRow, nextCol, curLength+1});
+                        visited[nextRow][nextCol] = true;
+                    }
+                }
             }
         }
 
         return result;
     }
 
-    static int[] dr = {0,1,1,1,0,-1,-1,-1};
-    static int[] dc = {1,1,0,-1,-1,-1,0,1};
-
-    public static void dfs(int r, int c, int[][] grid, boolean[][] visited, ArrayList<Integer> pathList) {
-
-        visited[r][c] = true;
-        System.out.println(r+","+c+"탐색");
-        if ((r == grid.length - 1) && (c == grid[0].length - 1)) {
-            System.out.println("경로찾았당");
-            return;
-        }
-        for (int i = 0; i < dr.length; i++) {
-            int currLow = r + dr[i];
-            int currCol = c + dc[i];
-            if (currLow >= 0 && currLow < grid.length && currCol >= 0 && currCol < grid[0].length) {
-                System.out.println(currLow+","+currCol+" 가보자고");
-                if (grid[currLow][currCol] == 0 && !visited[currLow][currCol]) {
-                    dfs(currLow, currCol, grid, visited, pathList);
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) {
-
         //shortestPathBinaryMatrix(new int[][]{{0,0,0},{1,1,0},{1,1,0}});
-        shortestPathBinaryMatrix(new int[][]{{0,0,0,0},{0,0,1,0},{0,0,1,0},{0,0,0,0}});
-
+        int result = shortestPathBinaryMatrix(new int[][]{{0,0,0,0},{0,0,1,0},{0,0,1,0},{0,0,0,0}});
+        System.out.println(result);
     }
 }
