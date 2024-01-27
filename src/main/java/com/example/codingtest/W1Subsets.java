e2db57417e0a548bff183fc298c124944d92266c
hyunzi <jhi271271@gmail.com>
2024-01-27 15:42:22 +0900

    Week 1 - BinarySearch Success

--- src/main/java/com/example/codingtest/W1Subsets.java
+++ src/main/java/com/example/codingtest/W1Subsets.java
@@ -17,16 +17,25 @@ public class W1Subsets {
     * [2]
     * [2,3]
     * [3]
-    * [1,2,3]
+    * [1,3]
     * */
     public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack();
+        backtrack(new ArrayList<Integer>(), nums, 0, result);
         return result;
     }
 
-    static void backtrack() {
+    static void backtrack(ArrayList<Integer> curr, int[] nums, int n, List<List<Integer>> result) {
+        /* 여기가 핵심인데, 조합과 다르게 정해진 원소 갯수 없이 모두 넣어야 하기 때문 */
+        result.add(new ArrayList<Integer>(curr));
 
+        for (int i = n; i < nums.length; i++) {
+            if (!curr.contains(nums[i])) {
+                curr.add(nums[i]);
+                backtrack(curr, nums, i+1, result);
+                curr.remove(Integer.valueOf(nums[i]));
+            }
+        }
     }
     public static void main(String[] args) {
         List<List<Integer>> result = subsets(new int[]{1,2,3});
