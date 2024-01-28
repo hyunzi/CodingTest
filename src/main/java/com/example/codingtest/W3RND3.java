cdc93ce0cf265311fd85dd3e5afe181392a5b371
JU_HYUNZI <JU_HYUNZI@eland.co.kr>
2024-01-28 22:04:36 +0900

    Week 3 - 연구소3 Success & Time Exceeded => 시간초과 개선을 위해 조금 수정

--- src/main/java/com/example/codingtest/W3RND3.java
+++ src/main/java/com/example/codingtest/W3RND3.java
@@ -1,6 +1,8 @@
 package com.example.codingtest;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class W3RND3 {
@@ -29,30 +31,29 @@ public class W3RND3 {
         //0이 없을 때까지 복제하며 복제한 카운트를 구한다
         //다 퍼뜨릴 수 없는 경우는 어떻게 구할까?
 
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        int mapLength = Integer.parseInt(str.split(" ")[0]);
-        int virusNumber = Integer.parseInt(str.split(" ")[1]);
-        String[][] graph = new String[mapLength][mapLength];
-        int idx = 0;
-        while (idx < mapLength) {
-            str = sc.nextLine();
-            graph[idx++] = str.split(" ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = reader.readLine().split(" ");
+        int mapLength = Integer.parseInt(line[0]);
+        int virusNumber = Integer.parseInt(line[1]);
+
+        char[][] graph = new char[mapLength][mapLength];
+        ArrayList<int[]> virusList = new ArrayList<>();
+        for (int r = 0; r < mapLength; r++) {
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            for (int c = 0; c < mapLength; c++) {
+                graph[r][c] = st.nextToken().charAt(0);
+                //1. 바이러스의 좌표를 모두 구해야하지 않을까..
+                switch (graph[r][c]) {
+                    //case '0': target++; break;
+                    case '2': virusList.add(new int[]{r,c});
+                }
+            }
         }
 
         int result = Integer.MAX_VALUE;
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
 
-        //1. 바이러스의 좌표를 모두 구해야하지 않을까..
-        ArrayList<int[]> virusList = new ArrayList<>();
-        for (int i = 0; i < mapLength; i++) {
-            for (int j = 0; j < mapLength; j++) {
-                if (graph[i][j].equals("2")) {
-                    virusList.add(new int[]{i,j});
-                }
-            }
-        }
 
         //2. 그다음 좌표의 부분조합을 구함
         //(e.g. 5개의 노드 중 3개를 선택하는 경우의 수)
@@ -74,7 +75,7 @@ public class W3RND3 {
                 queue.add(new int[]{curRow, curCol, 0});
             }
 
-            //바이러스 활성화 한 좌표에서 BFS
+            //4. 바이러스 활성화 한 좌표에서 BFS
             int depth = 0;
             while (!queue.isEmpty()) {
                 int[] currNode = queue.poll();
@@ -85,8 +86,8 @@ public class W3RND3 {
                     int nextRow = currRow + dr[i];
                     int nextCol = currCol + dc[i];
                     if (nextRow >= 0 && nextRow < mapLength && nextCol >= 0 && nextCol < mapLength) {
-                        //그자리에서 상하좌우 보면서 visited 안했는데 0이라 바이러스 옮길 수 있으면?
-                        if (graph[nextRow][nextCol].equals("0") && !visited[nextRow][nextCol]) {
+                        //그 자리에서 상하좌우 보면서 visited 안했는데 0이라 바이러스 옮길 수 있으면?
+                        if (graph[nextRow][nextCol] == '0' && !visited[nextRow][nextCol]) {
                             visited[nextRow][nextCol] = true;
                             queue.add(new int[]{nextRow, nextCol, depth+1});
                         }
@@ -97,10 +98,12 @@ public class W3RND3 {
             //만약에 visited에 1,2 아닌 곳에 방문하지 못했으면? -1 아니면 마지막 depth
             for (int i = 0; i < mapLength; i++) {
                 for (int j = 0; j < mapLength; j++) {
-                    if (graph[i][j].equals("0") && !visited[i][j]) {
+                    if (graph[i][j] == '0' && !visited[i][j]) {
                         depth = -1;
+                        break;
                     }
                 }
+                if (depth == -1) break;
             }
 
             if (depth != -1) {
