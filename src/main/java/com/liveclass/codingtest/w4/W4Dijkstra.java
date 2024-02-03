b25f911571b8ba29ef1ec6b953c5a6d079426687
JU_HYUNZI <JU_HYUNZI@eland.co.kr>
2024-02-03 17:32:21 +0900

    Week 4 - Dijkstra Sample

---
+++ src/main/java/com/liveclass/codingtest/w4/W4Dijkstra.java
@@ -0,0 +1,75 @@
+package com.liveclass.codingtest.w4;
+
+import java.util.*;
+
+public class W4Dijkstra {
+
+
+    /* https://school.programmers.co.kr/learn/courses/30/lessons/72413 */
+
+    private static int dijkstra(Map<Integer, List<Edge>> graph, int start, int end) {
+        final int INF = Integer.MAX_VALUE;
+        int[] distance = new int[graph.size() + 1];
+        Arrays.fill(distance, INF);
+
+        Queue<Entry> pq = new PriorityQueue<>();
+        pq.add(new Entry(start, 0));
+        distance[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Entry current = pq.remove();
+            if (distance[current.node] < current.distance) continue;
+
+            for (Edge edge : graph.get(current.node)) {
+                int newDist = current.distance + edge.distance;
+                if (newDist < distance[edge.to]) {
+                    distance[edge.to] = newDist;
+                    pq.add(new Entry(edge.to, newDist));
+                }
+            }
+        }
+
+        return distance[end];
+    }
+
+    public static void main(String[] args) {
+        Map<Integer, List<Edge>> graph= new HashMap<>() {{
+            put(1, List.of(new Edge(2, 2), new Edge(4, 1)));
+            put(2, List.of(new Edge(3, 2), new Edge(5, 2), new Edge(6, 4)));
+            put(3, List.of(new Edge(6, 4)));
+            put(4, List.of(new Edge(7, 5)));
+            put(5, List.of(new Edge(8, 1)));
+            put(6, List.of(new Edge(5, 3)));
+            put(7, List.of(new Edge(6, 7), new Edge(8, 6)));
+            put(8, List.of());
+        }};
+
+        int result = dijkstra(graph, 1,8);
+        System.out.println("result = " + result);
+    }
+
+    private static class Edge {
+        private int to;
+        private int distance;
+
+        public Edge(int to, int distance) {
+            this.to = to;
+            this.distance = distance;
+        }
+    }
+
+    private static class Entry implements Comparable<Entry> {
+        private int node;
+        private int distance;
+
+        public Entry(int node, int distance) {
+            this.node = node;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Entry o) {
+            return this.distance - o.distance;
+        }
+    }
+}
