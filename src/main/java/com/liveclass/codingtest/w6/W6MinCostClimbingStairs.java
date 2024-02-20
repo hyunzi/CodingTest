16a92a73b307bc76883844ee89b342326f3dc061
hyunzi <jhi271271@gmail.com>
2024-02-20 19:05:31 +0900

    Week 6 - Min Cost Climbing Stairs Success

---
+++ src/main/java/com/liveclass/codingtest/w6/W6MinCostClimbingStairs.java
@@ -0,0 +1,40 @@
+package com.liveclass.codingtest.w6;
+
+public class W6MinCostClimbingStairs {
+
+    /* https://leetcode.com/problems/min-cost-climbing-stairs/description/ */
+
+    public static int minCostClimbingStairs(int[] cost) {
+
+        //5,4,1,3,2,100,2,100,6
+        //1,5,1,1,4,...
+
+        //0,3 - 1칸 넘어가버리기
+        //1,3 - 2칸 넘어가버리기
+        //2,6 - 2,1칸 넘어가버리기
+        //3,6 - 2,2칸 넘어가버리기
+        //4,8 - 2,1,2칸 넘어가버리기
+        //5,8 - 2,1,2,1칸 넘어가버리기
+        //6,8 - 2,1,2,1칸 넘어가버리기
+        //7,16 - 2,1,2,2칸 넘어가버리기
+
+        int[] dp = new int[cost.length + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i = 2; i <= cost.length; i++) {
+            dp[i] = Math.min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
+        }
+
+        return dp[cost.length];
+
+    }
+
+    public static void main(String[] args) {
+
+        int result1 = minCostClimbingStairs(new int[]{10,15,20});
+        System.out.println("result1 = " + result1);
+        int result2 = minCostClimbingStairs(new int[]{1,100,1,1,1,100,1,1,100,1});
+        System.out.println("result2 = " + result2);
+    }
+}
