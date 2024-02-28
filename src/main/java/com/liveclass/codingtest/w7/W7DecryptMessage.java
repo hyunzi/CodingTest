de25acd2523bd3d440637a564ea0a524682afb84
hyunzi <jhi271271@gmail.com>
2024-02-28 18:00:01 +0900

    Week 7 - Decrypt Message

---
+++ src/main/java/com/liveclass/codingtest/w7/W7DecryptMessage.java
@@ -0,0 +1,29 @@
+package com.liveclass.codingtest.w7;
+
+public class W7DecryptMessage {
+
+    public static String solution(String m, String k) {
+
+        String result = "";
+        for (int i = 0, j = 0; i < m.length(); i++) {
+            if (j < k.length() && m.charAt(i) == k.charAt(j)) {
+                j++;
+            } else {
+                result += m.charAt(i);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        String result1 = solution("kkaxbycyz", "abc");
+        System.out.println("result1 = " + result1);
+        String result2 = solution("acbbcdc", "abc");
+        System.out.println("result2 = " + result2);
+        String result3 = solution("newtext", "et");
+        System.out.println("result3 = " + result3);
+
+    }
+}
