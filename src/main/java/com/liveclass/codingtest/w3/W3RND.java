09cf138588eada8910052c013072fd40f6ab4a4f
hyunzi <jhi271271@gmail.com>
2024-04-10 16:51:01 +0900

    Week 3 - 연구소 Success

--- src/main/java/com/liveclass/codingtest/w3/W3RND.java
+++ src/main/java/com/liveclass/codingtest/w3/W3RND.java
@@ -1,27 +1,155 @@
 package com.liveclass.codingtest.w3;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class W3RND {
 
 
+
+    /*
+    * [문제 분석]
+    * 1. 연구소는 직사각형 (크기 3~8)
+    * 2. 0:빈 칸(최소 3개) / 1:벽 / 2:바이러스(갯수 2~10)
+    * 3. 바이러스는 상하좌우 4개 방향으로 퍼질 수 있음
+    * 4. 벽을 3개 세워서 바이러스가 퍼진 후 빈 칸이 가장 많은 경우를 구해야함
+    * 5. 빈 칸의 갯수를 리턴한다
+    *
+    * [생각의 흐름]
+    * 1. 빈 칸에 3개의 벽을 세우는 모든 조합을 몇 개일까?
+    *   1-1.8*8=64, 64-2=62, (62!/59!3!)
+    *       62*61*60
+    *       62!/3!(62-3)!
+    *       226920/37820
+    * 순열의 경우의 수 226920을 3! 로 나누면 조합의 경우의 수 나옴
+    * 2. 최대 37820 경우의 수에 대해, 바이러스가 퍼지도록 DFS
+    * 3. DFS 종료된 후 0이 몇 개 인지 구하여 Math.max
+    *
+    * [코드 구현]
+    * 1. 데이터 입력 받으며 빈 칸의 좌표 저장
+    * 2. 빈 칸의 좌표로 combination
+    * 3. 각 comb 탐색
+    * 3-1. comb[i] 의 3개 좌표를 1로 치환
+    * 3-2. 2(바이러스) 좌표에서 DFS
+    * 3-3. 0의 갯수 확인
+    * 4. 0이 최대인 갯수 리턴
+    * */
+    static char[][] graph;
+    static List<int[]> emptyList, virusList;
+
     public static void main(String[] args) throws IOException {
 
         /* 1. 데이터 입력 받기 */
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        int mapLength = Integer.parseInt(str.split(" ")[0]);
-        int virusNumber = Integer.parseInt(str.split(" ")[1]);
-        String[][] graph = new String[mapLength][mapLength];
-        int idx = 0;
-        while (idx < mapLength) {
-            graph[idx++] = sc.nextLine().split(" ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = reader.readLine().split(" ");
+        int row = Integer.parseInt(line[0]);
+        int col = Integer.parseInt(line[1]);
+        graph = new char[row][col];
+        emptyList = new ArrayList<>();
+        virusList = new ArrayList<>();
+
+        for (int r = 0; r < row; r++) {
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            for (int c = 0; c < col; c++) {
+                graph[r][c] = st.nextToken().charAt(0);
+                if (graph[r][c] == '0') emptyList.add(new int[]{r,c});
+                if (graph[r][c] == '2') virusList.add(new int[]{r,c});
+            }
         }
 
-        //만약에 2가 10개더라도, 2의 상하좌우에 벽을 3개 세우는 케이스는
-        //10 * 4 = 40개 중에 3개씩 부분조합을 구하는 경우임
-        //이 경우의 수를 모두 탐색..?
-        //헉 더 많이... 0이 있는 모든 곳의 부분조합을 구하는 방법으로 풀어야함 ㄷㄷ
+        List<List<int[]>> combinations = new ArrayList<>();
+        List<int[]> arr = new ArrayList<>();
+        combination(0, emptyList.size(), 3, arr, combinations);
+
+        int result = 0;
+        for (List<int[]> combination : combinations) {
+            char[][] map = new char[row][col];
+            for (int i = 0; i < row; i++) {
+                System.arraycopy(graph[i],0,map[i],0, col);
+            }
+
+           for (int[] comb : combination) {
+               //comb[0], comb[1] 좌표를 1로 치환
+               map[comb[0]][comb[1]] = '1';
+           }
+
+            boolean[][] visited = new boolean[row][col];
+            for (int[] virus : virusList) {
+               bfs(virus, map, visited);
+           }
+           result = Math.max(result, getCountEmptyCell(map));
+
+            for (int[] comb : combination) {
+                //comb[0], comb[1] 좌표를 0로 치환
+                map[comb[0]][comb[1]] = '0';
+            }
+        }
+        System.out.println(result);
     }
+
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    public static void bfs(int[] startNode, char[][] map, boolean[][] visited) {
+
+        int r = startNode[0];
+        int c = startNode[1];
+        visited[r][c] = true;
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{r,c});
+
+        while (!queue.isEmpty()) {
+            int[] currNode = queue.poll();
+
+            for (int i = 0; i < dr.length; i++) {
+                int currRow = currNode[0] + dr[i];
+                currRow = currRow < 0 ? 0 : currRow;
+                currRow = currRow >= map.length-1 ? map.length-1 : currRow;
+                int currCol = currNode[1] + dc[i];
+                currCol = currCol < 0 ? 0 : currCol;
+                currCol = currCol >= map[0].length-1 ? map[0].length-1 : currCol;
+
+                if (map[currRow][currCol] == '0' && !visited[currRow][currCol]) {
+                    visited[currRow][currCol] = true;
+                    map[currRow][currCol] = '2';
+                    queue.add(new int[]{currRow, currCol});
+                }
+            }
+        }
+
+    }
+
+    public static int getCountEmptyCell(char[][] map) {
+        int result = 0;
+        for (char[] row : map) {
+            for(char c : row) {
+                if (c == '0') result++;    
+            }
+        }    
+        return result;
+    }
+    
+    public static void combination(int start, int n, int r, List<int[]> arr, List<List<int[]>> combList) {
+        if (arr.size() >= r) {
+            combList.add(new ArrayList<>(arr));
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            arr.add(emptyList.get(i));
+            combination(i+1, n, r, arr, combList);
+            arr.remove(emptyList.get(i));
+        }
+    }
+
+/* 데이터 예시
+4 6
+0 0 0 0 0 0
+1 0 0 0 0 2
+1 1 1 0 0 2
+0 0 0 0 0 2
+
+*/
+
 }
