47e362248c8427f6cd709ae2b9bbe8aace0a127a
hyunzi <jhi271271@gmail.com>
2024-04-10 12:55:38 +0900

    Week 2 - Coin Change, Sheep And Wolf - Recap

--- src/main/java/com/liveclass/codingtest/w2/W2CoinChangeNew.java
+++ src/main/java/com/liveclass/codingtest/w2/W2CoinChangeNew.java
@@ -9,19 +9,20 @@ public class W2CoinChangeNew {
     public static int coinChange(int[] coins, int amount) {
 
         if (amount == 0) return 0;
-        boolean visited[] = new boolean[amount+1];
-        Queue<int[]> queue = new ArrayDeque<int[]>();
+
+        boolean[] visited = new boolean[amount + 1];
+        Queue<int[]> queue = new ArrayDeque<>();
         queue.add(new int[]{amount, 0});
 
         while (!queue.isEmpty()) {
-
             int[] cur = queue.poll();
 
-            for (int i = 0; i < coins.length; i++) {
-                int nextAmount = cur[0] - coins[i];
+            for (int coin : coins) {
+                int nextAmount = cur[0] - coin;
                 if (nextAmount == 0) {
-                    return cur[1] + 1;
-                } else if (nextAmount > 0 && !visited[nextAmount]){
+                    return cur[1] + 1; //이걸 for 문 밖으로 빼면 오버플로우가 발생하는 경우가 생김
+                    // integer.MAXVALUE 값 때문에!!
+                } else if (nextAmount > 0 && !visited[nextAmount]) {
                     //위 조건문에서 순서를 바꾸면 visited[-1] 이런 식으로 들어갈 수 있어서 오류..
                     queue.add(new int[]{nextAmount, cur[1] + 1});
                     visited[nextAmount] = true;
@@ -32,12 +33,11 @@ public class W2CoinChangeNew {
     }
 
     public static void main(String[] args) {
-        System.out.println("결과: "+coinChange(new int[]{3,4,5}, 10));
-        System.out.println("결과: "+coinChange(new int[]{2}, 3));
-        System.out.println("결과: "+coinChange(new int[]{1,2,3}, 15));
-        System.out.println("결과: "+coinChange(new int[]{1,1,1}, 15));
+        System.out.println("결과: " + coinChange(new int[]{3, 4, 5}, 10));
+        System.out.println("결과: " + coinChange(new int[]{2}, 3));
+        System.out.println("결과: " + coinChange(new int[]{1, 2, 3}, 15));
+        System.out.println("결과: " + coinChange(new int[]{1, 1, 1}, 15));
     }
-
 
     // 안쓰고 그냥 배열이 더 편함..
     static class Entry {
--- src/main/java/com/liveclass/codingtest/w2/W2SheepAndWolfNew.java
+++ src/main/java/com/liveclass/codingtest/w2/W2SheepAndWolfNew.java
@@ -7,32 +7,32 @@ public class W2SheepAndWolfNew {
     /* Site URL: https://school.programmers.co.kr/learn/courses/30/lessons/92343 */
 
     /*
-    * [문제 분석]
-    * 1. info[0,0,1,1,1,...] : 양/양/늑/늑/늑 정보를 담고 있는 배열
-    * 2. edge[{0,1},{1,2},{1,4},]: 이진트리를 나타냄
-    *
-    * [생각의 흐름]
-    * 1. 백트래킹 써야할 듯 진행하다가 아니면 되돌아 오는
-    * 2. 루트에서 출발, 다음 이동할 곳의 양/늑 카운트 했을 때 양 <= 늑 이면 진행하면 안됨
-    * 3. 양 > 늑 경우로만 진행
-    *
-    * [코드 설계]
-    * 1. visited 배열 선언 (노드의 사이즈만큼)
-    * 2. 0번 노드 visit 체크
-    * 3. DFS + 백트래킹
-    * 4. 모든 edges 를 돌면서 확인
-    *   4-1. edge[부모엣지, 자식엣지] 에서 부모엣지 visit 이면서 자식엣지 !visit 인 경우 진행
-    *   4-2. 백트래킹
-    *      4-2-1. 자식노드 방문표시
-    *      4-2-2. 양이라면 양 + 1 해서 dfs 진행
-    *      4-2-3. 늑대라면 늑대 + 1 해서 dfs 진행
-    *      4-2-4. 자식노드 방문표시 해제
-    *      4-2-5. dfs 의 결과는 maxSheep
-    * */
+     * [문제 분석]
+     * 1. info[0,0,1,1,1,...] : 양/양/늑/늑/늑 정보를 담고 있는 배열
+     * 2. edge[{0,1},{1,2},{1,4},]: 이진트리를 나타냄
+     *
+     * [생각의 흐름]
+     * 1. 백트래킹 써야할 듯 진행하다가 아니면 되돌아 오는
+     * 2. 루트에서 출발, 다음 이동할 곳의 양/늑 카운트 했을 때 양 <= 늑 이면 진행하면 안됨
+     * 3. 양 > 늑 경우로만 진행
+     *
+     * [코드 설계]
+     * 1. visited 배열 선언 (노드의 사이즈만큼)
+     * 2. 0번 노드 visit 체크
+     * 3. DFS + 백트래킹
+     * 4. 모든 edges 를 돌면서 확인
+     *   4-1. edge[부모엣지, 자식엣지] 에서 부모엣지 visit 이면서 자식엣지 !visit 인 경우 진행
+     *   4-2. 백트래킹
+     *      4-2-1. 자식노드 방문표시
+     *      4-2-2. 양이라면 양 + 1 해서 dfs 진행
+     *      4-2-3. 늑대라면 늑대 + 1 해서 dfs 진행
+     *      4-2-4. 자식노드 방문표시 해제
+     *      4-2-5. dfs 의 결과는 maxSheep
+     * */
 
     public static int sheepAndWolf(int[] info, int[][] edge) {
 
-        boolean visited[] = new boolean[info.length];
+        boolean[] visited = new boolean[info.length];
         visited[0] = true;
         return dfs(info, edge, visited, 1, 0);
     }
@@ -49,20 +49,21 @@ public class W2SheepAndWolfNew {
             if (visited[parent] && !visited[child]) {
                 visited[child] = true;
                 if (info[child] == 0) {
-                    maxSheep = Math.max(maxSheep, dfs(info, edges, visited, sheep+1, wolf));
+                    maxSheep = Math.max(maxSheep, dfs(info, edges, visited, sheep + 1, wolf));
                 } else {
-                    maxSheep = Math.max(maxSheep, dfs(info, edges, visited, sheep, wolf+1));
+                    maxSheep = Math.max(maxSheep, dfs(info, edges, visited, sheep, wolf + 1));
                 }
+                visited[child] = false;
             }
         }
 
         return maxSheep;
     }
-    
+
     public static void main(String[] args) {
-        int result1 = sheepAndWolf(new int[]{0,0,1,1,1,0,1,0,1,0,1,1}, new int[][]{{0,1},{1,2},{1,4},{0,8},{8,7},{9,10},{9,11},{4,3},{6,5},{4,6},{8,9}});
+        int result1 = sheepAndWolf(new int[]{0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1}, new int[][]{{0, 1}, {1, 2}, {1, 4}, {0, 8}, {8, 7}, {9, 10}, {9, 11}, {4, 3}, {6, 5}, {4, 6}, {8, 9}});
         System.out.println("result1 = " + result1);
-        int result2 = sheepAndWolf(new int[]{0,1,0,1,1,0,1,0,0,1,0}, new int[][]{{0,1},{0,2},{1,3},{1,4},{2,5},{2,6},{3,7},{4,8},{6,9},{9,10}});
+        int result2 = sheepAndWolf(new int[]{0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0}, new int[][]{{0, 1}, {0, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 6}, {3, 7}, {4, 8}, {6, 9}, {9, 10}});
         System.out.println("result2 = " + result2);
     }
 }
